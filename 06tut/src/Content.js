fd4ac992f7329e3b60ba9323e78fdff9c842a575
Chris East <chris.east95@gmail.com>
2023-08-12 10:47:23 -0400

    Finished part 6 of tut

--- 06tut/src/Content.js
+++ 06tut/src/Content.js
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 export const Content = () => {
+    const [name, setName] = useState('Dave');
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () => {
         const names = ["Bob",'Dave','Kevin']
         const int = Math.floor(Math.random() * 3);
-        return names[int]
+        setName(names[int])
     }
 
     const handleClick = () =>{
-        console.log("You clicked it!");
+        setCount(count + 1)
+        console.log(count);
     }
 
-    const handleClick2 = (name) =>{
-        console.log(`${name} was clicked!`);
-    }
-
-    const handleClick3 = (e) =>{
-        console.log(e.target);
+    const handleClick2 = () =>{
+        console.log(count);
     }
 
     return (
         <main>
             <p onDoubleClick={handleClick}>
-                Hello {handleNameChange()}!
+                Hello {name}!
             </p>
+            <button onClick={handleNameChange}>Change Name!</button>
             <button onClick={handleClick}>Click It!</button>
-            <button onClick={() => handleClick2('Dave')}>Click It!</button>
-            <button onClick={(e) => handleClick3(e)}>Click It!</button>
+            <button onClick={handleClick2}>Click It!</button>
         </main>
     )
 }
