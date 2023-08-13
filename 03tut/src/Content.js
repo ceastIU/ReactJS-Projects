04e77016721b5f58e830ead1a9aebd41cf0d0b42
Chris East <chris.east95@gmail.com>
2023-08-13 14:04:31 -0400

    Adding in old content from part 3

--- 03tut/src/Content.js
+++ 03tut/src/Content.js
@@ -7,11 +7,26 @@ export const Content = () => {
         return names[int]
     }
 
+    const handleClick = () =>{
+        console.log("You clicked it!");
+    }
+
+    const handleClick2 = (name) =>{
+        console.log(`${name} was clicked!`);
+    }
+
+    const handleClick3 = (e) =>{
+        console.log(e.target);
+    }
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click It!</button>
+            <button onClick={() => handleClick2('Dave')}>Click It!</button>
+            <button onClick={(e) => handleClick3(e)}>Click It!</button>
         </main>
     )
 }
