9145b4df858ed6229f49c0760cd5f84651419e71
Chris East <chris.east95@gmail.com>
2023-08-16 16:40:05 -0400

    Mid way through part 16 of the tut

--- 16tut/src/App.js
+++ 16tut/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Nav from "./Nav"
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes, useHistory } from 'react-router-dom';
+import { useState, useEffect } from "react";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Home />} ></Route>
+        <Route path="/post" element={<NewPost/>}></Route>
+        <Route path="/post/:id" Component={PostPage} />
+        <Route exact path='/about' element={< About />}></Route>
+        <Route path="*" Component={Missing} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
