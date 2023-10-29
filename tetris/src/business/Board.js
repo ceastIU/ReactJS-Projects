d7ef5d574cb2bb38a22559aaedbd2fcb60107a9d
Chris East <chris.east95@gmail.com>
2023-10-29 16:38:27 -0400

    Added player to the board

--- tetris/src/business/Board.js
+++ tetris/src/business/Board.js
@@ -1,20 +1,36 @@
 import { defaultCell } from "./Cell";
+import { transferToBoard } from "./Tetrominoes"
 
-export const buildBoard = ({rows, columns}) => {
-    const builtRows = Array.from({ length: rows}, () => 
-        Array.from({length: columns}, () => ({...defaultCell}))
+export const buildBoard = ({ rows, columns }) => {
+    const builtRows = Array.from({ length: rows }, () =>
+      Array.from({ length: columns }, () => ({ ...defaultCell }))
     );
+  
+    return {
+      rows: builtRows,
+      size: { rows, columns }
+    };
+  };
+
+  export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }) => {
+    const { tetromino, position } = player;
+  
+    // Copy and clear spaces used by pieces that
+    // hadn't collided and occupied spaces permanently
+    let rows = board.rows.map((row) =>
+      row.map((cell) => (cell.occupied ? cell : { ...defaultCell }))
+    );
+
+    rows = transferToBoard({
+        className: tetromino.className,
+        isOccupied: player.collided,
+        position,
+        rows,
+        shape: tetromino.shape
+      });
 
     return {
-        rows: builtRows,
-        size: { rows, columns }
+        rows,
+        size: {...board.size }
     }
-}
-
-export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }) => {
-    const { tetromino, position } = player;
-
-    let rows = board.rows.map((row) => 
-        row.map((cell) => (cell.occupied ? cell : {...defaultCell}))    
-    );
 }
