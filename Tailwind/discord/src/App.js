f7f5bff0c7c3644bed2c9fc4d4bca7b4fb6b322c
Chris East <chris.east95@gmail.com>
2023-12-05 14:45:48 -0500

    Finished quick tutorial

---
+++ Tailwind/discord/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SideBar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="flex">
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ Tailwind/discord/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+      },
+    },
+  },
+  plugins: [],
+};
